00f8461ec01ccc5d53b513af5d2f7d66983178c5
Linh To <salem@Linhs-MacBook-Pro-2.local>
2026-04-28 13:32:38 -0400

    Push all homework and notes from midterm to final proposal

---
+++ Midterm/Portfolio/hamburber.js
@@ -0,0 +1,17 @@
+function HamMenu() {
+  var x = document.getElementById("navbar");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+function DropDown() {
+  var x = document.getElementById("dropdown");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
