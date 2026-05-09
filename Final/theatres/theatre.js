733eb7a6c5df9cfccff2238f6bb125a8d108e1d0
Linh To <salem@Linhs-MacBook-Pro-2.local>
2026-05-09 12:42:51 -0400

    final website submission

---
+++ Final/theatres/theatre.js
@@ -0,0 +1,264 @@
+const theatre = [
+  {
+    title: "The Balusters",
+    img: "./img/thebalusters.jpg",
+    link: "https://www.manhattantheatreclub.com/shows/2025-26-season/the-balusters/?utm_source=google&utm_medium=cpc&https://www.manhattantheatreclub.com/shows/2022-23-season/the-best-we-could/?utm_source=google&utm_medium=search&utm_campaign=bestwecould&gclsrc=aw.ds&gad_source=1&gad_campaignid=23624282283&gbraid=0AAAAADQ8MHrU3X5-NNvYfuzaLA546-tO9&gclid=CjwKCAjwqubPBhBOEiwAzgZX2sKd84kCH_8coXA7Go1bjTrgIEYuCoWPnMgBG4jiMpu1yltI-xOG7xoC2QEQAvD_BwE"
+  },
+  {
+    title: "Fallen Angels",
+    img: "./img/fallenangels.png",
+    link: "https://www.roundabouttheatre.org/get-tickets/2025-2026-season/fallen-angels/performances?utm_source=GoogleAds&utm_medium=PerformanceMax&utm_content=PreOpenCampaign&gad_source=1&gad_campaignid=23489551363&gbraid=0AAAAA94IdFwVfm4pBcz2aNDgqa37FKgME&gclid=CjwKCAjwqubPBhBOEiwAzgZX2qBQzUHbIRXsgtGJe3R_9ZC_4tpMdGdZIK68LLjomE5KUoozHoU1OxoCFgMQAvD_BwE"
+  },
+  {
+    title: "Becky Shaw",
+    img: "./img/beckyshaw.jpg",
+    link: "https://2st.com/shows/becky-shaw?gad_source=1&gad_campaignid=23551031679&gbraid=0AAAAABQOZiIsEjKALKuf15InmVRBw6DvA&gclid=CjwKCAjwqubPBhBOEiwAzgZX2pbpj403EztDQwHZvnytJPwk74OSaBuXIEujSHXAziqyrRwwha_K6RoCOioQAvD_BwE"
+  },
+  {
+    title: "Every Brilliant Thing",
+    img: "./img/radcliffe.jpg",
+    link: "https://everybrilliantthing.com/"
+  },
+  {
+    title: "Dog Day Afternoon",
+    img: "./img/dda.png",
+    link: "https://dogdayafternoon.com/?gad_source=1&gad_campaignid=23053473281&gbraid=0AAAAA_y8iP-QFdb3ADG9emasxWqP9Tbmk&gclid=CjwKCAjwqubPBhBOEiwAzgZX2qlB_uzvdsl8SzIIK85qa2sLwuvM0-Hyav2DAiH_XcLkr1fDkcyH3BoCtYoQAvD_BwE"
+  },
+  {
+    title: "Giant",
+    img: "./img/giant.jpg",
+    link: "https://gianttheplay.com/"
+  },
+  {
+    title: "The Fear of 13",
+    img: "./img/brody.jpg",
+    link: "https://thefearof13broadway.com/"
+  },
+  {
+    title: "Titanique",
+    img: "./img/titanique.jpg",
+    link: "https://titaniquebroadway.com/?gclsrc=aw.ds&gad_source=1&gad_campaignid=19996990075&gbraid=0AAAAABfU4Xvjt0rue6xzGq6eCiRWsB652&gclid=CjwKCAjwqubPBhBOEiwAzgZX2vQyG3uUQOoXCmUbQHvcPb9p2LFvQQ3Zt_xAzaZyEkP8kd953d2AzBoC4OoQAvD_BwE"
+  },
+  {
+    title: "Proof",
+    img: "./img/proof.png",
+    link: "https://proofbroadway.com/?gad_source=1&gad_campaignid=22926435331&gbraid=0AAAABBALsa0_98xWVe-DGNbpLCiStgHxC&gclid=CjwKCAjwqubPBhBOEiwAzgZX2r6pHCm4ra-27ERictXo6b2Aaqli17AofGndgUO1nYTnHk9Dw0j9XBoC5HUQAvD_BwE"
+  },
+  {
+    title: "The Rocky Horror Show",
+    img: "./img/rockyhorror.png",
+    link: "https://www.roundabouttheatre.org/get-tickets/2025-2026-season/rocky-horror?utm_source=GoogleAds&utm_medium=PaidSearch&utm_campaign=BrandedTerms_NYDMA&utm_content=BrandedTerms1&gad_source=1&gad_campaignid=23214919129&gbraid=0AAAAA94IdFxcpmxRtcHrljYLSu2uEXN3q&gclid=CjwKCAjwqubPBhBOEiwAzgZX2hL_vzaZaXEyekrim6rzgz2Ac-cCyNl08jQj-N3y4W-G5QvzyvbQTBoCk5wQAvD_BwE"
+  },
+  {
+    title: "Joe Turner's Come and Gone",
+   img: "./img/joeturner.jpg",
+    link: "https://joeturnerbway.com/"
+  },
+  {
+    title: "Ragtime",
+    img: "./img/ragtime.png",
+    link: "https://www.lct.org/shows/ragtime/?gad_source=1&gad_campaignid=23263195900&gbraid=0AAAABCB_Ux6tpycXMSjF6J_1P6ysQW-z-&gclid=CjwKCAjwqubPBhBOEiwAzgZX2ia1DgeEchxQTk0Ez3mpRZLFQYIMxBSdhyQIDBBmR0vKd0aJd3tWpRoCCPwQAvD_BwE"
+  },
+  {
+    title: "Death of a Salesman",
+    img: "./img/doas.png",
+    link: "https://salesmanbroadway.com/"
+  },
+  {
+    title: "Moulin Rouge!",
+    img: "./img/moulinrouge.jpg",
+    link: "https://moulinrougemusical.com/new-york/home/?utm_source=google&utm_medium=search&utm_campaign=moulinrouge_2026&utm_content=paid&gad_source=1&gad_campaignid=23359105840&gbraid=0AAAAADFmsqPUTTXGgDYHs9fXRcLZo_x-p&gclid=CjwKCAjwqubPBhBOEiwAzgZX2p0AZwzH4jo3Fjp5hLwWtLNeMezRxEJ51Cxdl15XAFQbx_msLOBAUxoChFkQAvD_BwE"
+  },
+  {
+    title: "Beaches: A New Musical",
+    img: "./img/beaches.jpg",
+    link: "https://beachesthemusical.com/"
+  },
+  {
+    title: "Schmigadoon!",
+    img: "./img/schmigadoon.jpg",
+    link: "https://schmigadoonbroadway.com/?gad_source=1&gad_campaignid=23117021268&gbraid=0AAAABBMUHcSqkoTYHEflut0g9z4Rzj_jU&gclid=CjwKCAjwqubPBhBOEiwAzgZX2sJnTdXkCPks3LV_-7MOJLOlHBUv-A1ZmB-oRY9mkBIl_hlispU1hBoCO6gQAvD_BwE"
+  },
+  {
+    title: "The Lost Boys",
+    img: "./img/lostboys.jpg",
+    link: "https://www.lostboysmusical.com/?utm_source=google&utm_medium=search&utm_campaign=thelostboys_2026&utm_content=paid&gad_source=1&gad_campaignid=23371266424&gbraid=0AAAABB4FBntWauSb-cymRdFLn_PetMoYN&gclid=CjwKCAjwqubPBhBOEiwAzgZX2rCbJ1sD-RP6MWXvWLPe1r9xJ57TwkSqNgoRaR7PYbsVnThGYMgE2hoCWDMQAvD_BwE"
+  },
+  {
+    title: "Chess",
+    img: "./img/chess.jpg",
+    link: "https://chessbroadway.com/?utm_source=GoogleAds&utm_medium=PaidSearch&utm_campaign=BrandedTerms&gad_source=1&gad_campaignid=22721720144&gbraid=0AAAAA_ktFSbnLC02VnMenFk9lDCVhZGV0&gclid=CjwKCAjwqubPBhBOEiwAzgZX2mWjBY0yt3sASp1E8q__caCX2rdDNVOl6rRZAyMyYjUxF1iz27pIqBoCYFYQAvD_BwE"
+  },
+  {
+    title: "Cats: The Jellicle Ball",
+    img: "./img/cats.png",
+    link: "https://catsthejellicleball.com/?utm_source=google&utm_medium=cpc&utm_campaignid=23087567655&utm_adgroupid=187150537592&utm_adid=777914800958&utm_term=cats%20the%20jellicle%20ball&utm_matchtype=b&utm_campaign&utm_adgroup&gad_source=1&gad_campaignid=23087567655&gbraid=0AAAAA_KCfwdcUoi49FonRql0I4PuFEBsf&gclid=CjwKCAjwqubPBhBOEiwAzgZX2okNPZMA12XIoS60g1sYZW3-cZC0JpE9KrDnU9UMCPByfYHHd6kQwhoCOvIQAvD_BwE"
+  },
+  {
+    title: "Chicago",
+    img: "./img/chicago.jpg",
+    link: "https://chicagothemusical.com/?utm_source=google&utm_medium=search&utm_campaign=chicago_2026&utm_content=paid&gad_source=1&gad_campaignid=23377194669&gbraid=0AAAAADMPY2Gu_B6TSXFkgiqMigcBI9q2t&gclid=CjwKCAjwqubPBhBOEiwAzgZX2k48YRkCbXeRi3KJ4UGZ1yabo9TkwnXe-RhhOfpE1pY7jWTSh6EyCBoCOaMQAvD_BwE"
+  },
+  {
+    title: "The Outsiders",
+    img: "./img/outsiders.jpg",
+    link: "https://outsidersmusical.com/?utm_source=google&utm_medium=search&gclsrc=aw.ds&gad_source=1&gad_campaignid=20889736052&gbraid=0AAAAAqnTh6jAQmKi3_tw0eyzP6w0Aoh1Z&gclid=CjwKCAjwqubPBhBOEiwAzgZX2rAT1LupcqMndF-l54n6CtClAQiCgChIpdsMTgEmLes5bfDS-NrTYxoC7kkQAvD_BwE"
+  },
+  {
+    title: "Two Strangers (Carry A Cake Across New York)",
+    img: "./img/twostrangers.jpg",
+    link: "https://twostrangersmusical.com/?gad_source=1&gad_campaignid=22874725581&gbraid=0AAAABArfTFSinR-TCA-opxzsd-6RNf-nn&gclid=CjwKCAjwqubPBhBOEiwAzgZX2hc-ElAvIJE3jKMtSb7PedR7fqugKO2yofIrerCHXlLR0UGTOHH9IBoCYJcQAvD_BwE"
+  },
+  {
+    title: "Just In Time",
+    img: "./img/justintime.jpg",
+    link: "https://justintimebroadway.com/"
+  },
+  {
+    title: "Book of Mormon",
+    img: "./img/mormon.jpg",
+    link: "https://bookofmormonbroadway.com/"
+  },
+  {
+    title: "Wicked",
+    img: "./img/wicked.jpg",
+    link: "https://wickedthemusical.com/tickets/?gclsrc=aw.ds&gad_source=1&gad_campaignid=13039528740&gbraid=0AAAAADi5kvR0GOfNYs0234brkK8qoRWaQ&gclid=CjwKCAjwqubPBhBOEiwAzgZX2vz9A3bwl7f845blle7qR2U9_gv8CPXsUo5x8ztsvHdatTEwHfnrxBoCRcwQAvD_BwE#broadway?utm_source=google&utm_medium=search"
+  },
+  {
+    title: "MJ The Musical",
+    img: "./img/mj.png",
+    link: "https://newyork.mjthemusical.com/?gclsrc=aw.ds&gad_source=1&gad_campaignid=20938190119&gbraid=0AAAAACjRt6mVTx72cY1bN1g3CL-Zqjsv2&gclid=CjwKCAjwqubPBhBOEiwAzgZX2lzmIwTTjyjPXr8DFrsNucILhvvxO353UsbVgLhv7-egCMG8LYk-fRoC7-8QAvD_BwE"
+  },
+  {
+    title: "Stranger Things: The First Shadow",
+    img: "./img/strangerthings.jpg",
+    link: "https://broadway.strangerthingsonstage.com/"
+  },
+  {
+    title: "Harry Potter And The Cursed Child",
+    img: "./img/harrypotter.jpg",
+    link: "https://broadway.harrypottertheplay.com/?gad_source=1&gad_campaignid=21422921264&gbraid=0AAAAADLYWnSjQBOv9yrbVxqlv1zAmBw8M&gclid=CjwKCAjwqubPBhBOEiwAzgZX2nK7dEIBTyOlgxNINinw-jQZvwSMiYHtuoG8lmce7g4qpLervhQVDRoCMvQQAvD_BwE"
+  },
+  {
+    title: "Operation Mincemeat: A New Musical",
+    img: "./img/mincemeat.jpg",
+    link: "https://operationbroadway.com/?gclsrc=aw.ds&gad_source=1&gad_campaignid=21767896034&gbraid=0AAAAA-Cjem6V3UJXQjP7xXcy6T4mPJBbQ&gclid=CjwKCAjwqubPBhBOEiwAzgZX2g7XHL9lO35i_pUm2aJuqWKTQv7SegzYHAbi3AFWeOq_LQVvYyf-jxoCC4sQAvD_BwE"
+  },
+  {
+    title: "Buena Vista Social Club",
+    img: "./img/buenavista.jpg",
+    link: "https://buenavistamusical.com/?gad_source=1&gad_campaignid=23315265280&gbraid=0AAAAA93JRmYsjhW4EpO-TsF0T5IDUL9VN&gclid=CjwKCAjwqubPBhBOEiwAzgZX2ltyDcHy6DstHMy7dyQL2Vx8tuecqAnIKj-j_JiLr1v75-ft9OKTsBoCD9AQAvD_BwE"
+  },
+  {
+    title: "Oh, Mary",
+    img: "./img/ohmary.jpg",
+    link: "https://www.ohmaryplay.com/?utm_source=google&utm_medium=search&utm_campaign=ohmary_2026&utm_content=paid&gad_source=1&gad_campaignid=23411139313&gbraid=0AAAAA9iLUC1_mF-cRpoh9no6FIWLtxNjE&gclid=CjwKCAjwqubPBhBOEiwAzgZX2qRf9730WLfK0AzsV4pBXzxkmBI8N-ig3bpiUDPJNZrI9z6-8-1v2hoCgE4QAvD_BwE"
+  },
+  {
+    title: "Death Becomes Her",
+    img: "./img/dbh.jpg",
+    link: "https://deathbecomesher.com/?utm_source=google&utm_medium=search&utm_campaign=deathbecomesher_2026&utm_content=paid&gad_source=1&gad_campaignid=23406460973&gbraid=0AAAAAqGoX9KiYCmXfItBe08G7WY2DzzU-&gclid=CjwKCAjwqubPBhBOEiwAzgZX2gHxyctixevMJ-TP3Tvod4ByCSfw5JLrgbCyTcnzLEum8xUjl-rU8BoC7g4QAvD_BwE"
+  },
+  {
+    title: "The Great Gatsby",
+    img: "./img/gatsby.jpg",
+    link: "https://broadwaygatsby.com/"
+  },
+  {
+    title: "Maybe Happy Ending",
+    img: "./img/mhe.jpg",
+    link: "https://www.maybehappyending.com/?gclsrc=aw.ds&gad_source=1&gad_campaignid=21366293706&gbraid=0AAAAA9gfsvwVsSAVXokH1B1_FfpRcMFol&gclid=CjwKCAjwqubPBhBOEiwAzgZX2sJ2pfT15a5WVsnrik-ifyZxQDzMoi7wwt8Fz96jZ0XLpUNVXKhfohoCcQ4QAvD_BwE"
+  },
+  {
+    title: "Six",
+    img: "./img/six.jpg",
+    link: "https://sixonbroadway.com/?utm_source=google%20ads&utm_medium=search&gad_source=1&gad_campaignid=8707326397&gbraid=0AAAAACjNH7kXbefg0NpgfulYLpNofDGaG&gclid=CjwKCAjwqubPBhBOEiwAzgZX2gOl7Bo2_yDWx7UGPTS-Pz0Xkcn5eejnIpGawLa12i5E0FIxqzWB2xoCkfkQAvD_BwE"
+  },
+  {
+    title: "The Lion King",
+    img: "./img/lionking.jpg",
+    link: "https://lionking.com/tickets/?gclsrc=aw.ds&gad_source=1&gad_campaignid=19914055661&gbraid=0AAAAApYe0z6xakm1nyFMMAhVl-o9vtiTw&gclid=CjwKCAjwqubPBhBOEiwAzgZX2gLmAD0tO8SMqxkjzXPl4GHnTQiDYdgjTtbldle57MYnFzl-cf4HnhoCxHMQAvD_BwE"
+  },
+  {
+    title: "Aladdin",
+    img: "./img/aladdin.jpg",
+    link: "https://aladdinthemusical.com/?utm_source=google&utm_medium=search&gclsrc=aw.ds&gad_source=1&gad_campaignid=19913964227&gbraid=0AAAAApYeHxYwS2B4g9hlrsP4ZGzaG9cWh&gclid=CjwKCAjwqubPBhBOEiwAzgZX2tucs7qjGd7MgyyzIWqu7HFBNncVu2b8JRkp9jKyFlzY-nKGFsVKeRoCqLAQAvD_BwE"
+  },
+  {
+    title: "Hamilton",
+    img: "./img/hamilton.jpg",
+    link: "https://hamiltonmusical.com/new-york/tickets/?utm_source=google&utm_medium=search&utm_campaign=hamilton_2026&utm_content=paid&gad_source=1&gad_campaignid=23350382544&gbraid=0AAAAADD32xEJ6RHoi9-fxAdX1fvdAfgoW&gclid=CjwKCAjwqubPBhBOEiwAzgZX2sF-jp9Q5NYl7o-aszkk5e5ThjMghaCbsxkL-X5xpGPnqaczQOU5rxoCO8wQAvD_BwE"
+  },
+  {
+    title: "& Juliet",
+    img: "./img/juliet.jpg",
+    link: "https://andjulietbroadway.com/tickets/?gad_source=1&gad_campaignid=21488939752&gbraid=0AAAAAoxGcYA-C0s5odXZ1-hJw_w55KKNp&gclid=CjwKCAjwqubPBhBOEiwAzgZX2mKbye8MQadpCqFMiJo_r1oDLcqsJfxllFeMdmk-1ZbkoleidMlXwRoCWCYQAvD_BwE#broadway"
+  },
+  {
+    title: "Hadestown",
+    img: "./img/hadestown.jpg",
+    link: "https://hadestown.com/?utm_source=GoogleAds&utm_medium=PaidSearch&gad_source=1&gad_campaignid=20392442602&gbraid=0AAAAAp97_pYU8TAURH61IP3v-ped3dE_s&gclid=CjwKCAjwqubPBhBOEiwAzgZX2huBBhEwePGDY1SlMAVdmDaHvQPlChHgFg9jEvRf9mSxFDiNTdEZ0xoCuvcQAvD_BwE"
+  },
+  
+];
+
+// TITLE CATEGORIES
+const categories = [
+  "Just Added",
+  "Popular",
+  "Recommended"
+];
+
+const container = document.getElementById('rowsContainer');
+
+// SHUFFLE FUNCTION
+function shuffleArray(array) {
+  const shuffled = [...array];
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+  return shuffled;
+}
+
+function createRow(title, index) {
+  const rowId = `row-${index}`;
+
+  const shows = shuffleArray(theatre).slice(0, 12);
+
+  return `
+    <div class="row">
+
+    <div class="category_title">
+      <h3>${title}</h3>
+    </div>
+
+      <div class="carousel-wrapper">
+        <div id="${rowId}" class="carousel">
+          ${shows.map(show => `
+            
+            <a class="card" href="${show.link}" target="_blank" rel="noopener noreferrer">
+              <img src="${show.img}" alt="${show.title}">
+            </a>
+
+          `).join('')}
+        </div>
+      </div>
+    </div>
+  `
+}
+
+// SCROLL FUNCTION
+function scrollRow(rowId, amount) {
+  const row = document.getElementById(rowId);
+  row.scrollBy({ left: amount, behavior: 'smooth' });
+}
+
+// RENDER ALL ROWS
+function renderRows() {
+  container.innerHTML = categories
+    .map((cat, i) => createRow(cat, i))
+    .join('');
+}
+
+renderRows();
